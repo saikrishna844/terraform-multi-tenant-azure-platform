e162a9771a19310ad0f16bf86772a6839c49e8d0
Sai Krishna <152583402+saikrishna844@users.noreply.github.com>
2026-08-05 07:36:03 +0530

    Format Terraform code

--- backend.tf
+++ backend.tf
@@ -1,5 +1,5 @@
-provider "azurerm" { 
-  features {} 
+provider "azurerm" {
+  features {}
 
   subscription_id = "31e04be6-4545-4ac3-88c4-cc79f284f7ac"
   tenant_id       = "d77ec4e8-c88b-431b-8d7f-77755a4bbefc"
